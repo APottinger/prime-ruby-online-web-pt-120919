91946f94a1cae8d9dcd75f9bf2b5e2f6cd05c064
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 01:36:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers.find do |num|
   num.prime?
-  if prime
-  puts "true"
-  if !prime
-    puts "false"
 end
 
+if prime
+  puts "true"
+if !prime
+    puts "false"
